0aad7f136cc35753d6cffe2aeeb58015806a303e
Pau Colomé <paucolome10@gmail.com>
2025-05-31 16:14:29 +0200

    nose

--- packages/app/src/app.tsx
+++ packages/app/src/app.tsx
@@ -1,5 +1,51 @@
-import { useState } from "firejs";
+import { createElement, render, useState } from "firejs";
+
+import './app.css';
 
 export function App() {
-    const [count, setCount] = useState(0);
+	const [val, setVal] = useState(1);
+
+	return createElement(
+		"div",
+		{
+			style:
+				"display: flex; align-items: center; justify-content: center; flex-direction: column; gap: 1em; height: 100vh; font-family: Vodafone Rg, Helvetica;",
+		},
+		// @ts-ignore
+		createElement(
+			"h1",
+			{
+				style: "font-family: Vodafone Rg, Helvetica; font-weight: 200;",
+			},
+			`value is ${val}`,
+		),
+		createElement(
+			"div",
+			{
+				style:
+					"display: flex; gap: 1em; align-items: center; justify-content: center; flex-direction: row",
+			},
+			// @ts-ignore
+			createElement(
+				"button",
+				{
+					onClick: () => setVal(value => value - 1),
+					style:
+						"font-family: Vodafone Rg, Helvetica; background-color: red; border-radius: 6px; border: none; color: white; padding: 1rem; cursor: pointer; font-size: 1em",
+				},
+				"Decrease",
+			),
+			createElement(
+				"button",
+				{
+					onClick: () => setVal(value => value + 1),
+					style:
+						"font-family: Vodafone Rg, Helvetica; background-color: red; border-radius: 6px; border: none; color: white; padding: 1rem; cursor: pointer; font-size: 1em",
+				},
+				"Add",
+			),
+		),
+	);
 }
+
+
--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,52 +1,6 @@
-import { createElement, render, useState } from "firejs";
+import { App } from "./app";
+import { render } from "firejs";
 
 const container = document.getElementById("root");
 
-function App() {
-	const [val, setVal] = useState(1);
-
-	return createElement(
-		"div",
-		{
-			style:
-				"display: flex; align-items: center; justify-content: center; flex-direction: column; gap: 1em; height: 100vh; font-family: Vodafone Rg, Helvetica;",
-		},
-		// @ts-ignore
-		createElement(
-			"h1",
-			{
-				style: "font-family: Vodafone Rg, Helvetica; font-weight: 200;",
-			},
-			`value is ${val}`,
-		),
-		createElement(
-			"div",
-			{
-				style:
-					"display: flex; gap: 1em; align-items: center; justify-content: center; flex-direction: row",
-			},
-			// @ts-ignore
-			createElement(
-				"button",
-				{
-					onClick: () => setVal(value => value - 1),
-					style:
-						"font-family: Vodafone Rg, Helvetica; background-color: red; border-radius: 6px; border: none; color: white; padding: 1rem; cursor: pointer; font-size: 1em",
-				},
-				"Decrease",
-			),
-			createElement(
-				"button",
-				{
-					onClick: () => setVal(value => value + 1),
-					style:
-						"font-family: Vodafone Rg, Helvetica; background-color: red; border-radius: 6px; border: none; color: white; padding: 1rem; cursor: pointer; font-size: 1em",
-				},
-				"Add",
-			),
-		),
-	);
-}
-
-// @ts-ignore
-render(createElement(App, {}), container);
+render(<App />, container);

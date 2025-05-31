49cdd79d7deeb217a12ab85fd7162920010d1e39
Pau Colomé <paucolome10@gmail.com>
2025-05-31 16:56:05 +0200

    refactor: simplify App component structure and enhance JSX runtime with new functions

--- packages/app/src/app.tsx
+++ packages/app/src/app.tsx
@@ -1,49 +1,31 @@
-import { createElement, useState } from "firejs";
+import { useState } from "firejs";
 
 import "./app.css";
 
 export function App() {
 	const [val, setVal] = useState(1);
 
-	return createElement(
-		"div",
-		{
-			style:
-				"display: flex; align-items: center; justify-content: center; flex-direction: column; gap: 1em; height: 100vh; font-family: Vodafone Rg, Helvetica;",
-		},
-		// @ts-ignore
-		createElement(
-			"h1",
-			{
-				style: "font-family: Vodafone Rg, Helvetica; font-weight: 200;",
-			},
-			`value is ${val}`,
-		),
-		createElement(
-			"div",
-			{
-				style:
-					"display: flex; gap: 1em; align-items: center; justify-content: center; flex-direction: row",
-			},
-			// @ts-ignore
-			createElement(
-				"button",
-				{
-					onClick: () => setVal(value => value - 1),
-					style:
-						"font-family: Vodafone Rg, Helvetica; background-color: red; border-radius: 6px; border: none; color: white; padding: 1rem; cursor: pointer; font-size: 1em",
-				},
-				"Decrease",
-			),
-			createElement(
-				"button",
-				{
-					onClick: () => setVal(value => value + 1),
-					style:
-						"font-family: Vodafone Rg, Helvetica; background-color: red; border-radius: 6px; border: none; color: white; padding: 1rem; cursor: pointer; font-size: 1em",
-				},
-				"Add",
-			),
-		),
+	return (
+		<div
+			style="display: flex; align-items: center; justify-content: center; flex-direction: column; gap: 1em; height: 100vh; font-family: Vodafone Rg, Helvetica;"
+		>
+			<h1 style="font-family: Vodafone Rg, Helvetica; font-weight: 200;">value is {val}</h1>
+			<div
+				style="display: flex; gap: 1em; align-items: center; justify-content: center; flex-direction: row"
+			>
+				<button
+					onClick={() => setVal(value => value - 1)}
+					style="font-family: Vodafone Rg, Helvetica; background-color: red; border-radius: 6px; border: none; color: white; padding: 1rem; cursor: pointer; font-size: 1em"
+				>
+					Decrease
+				</button>
+				<button
+					onClick={() => setVal(value => value + 1)}
+					style="font-family: Vodafone Rg, Helvetica; background-color: red; border-radius: 6px; border: none; color: white; padding: 1rem; cursor: pointer; font-size: 1em"
+				>
+					Add
+				</button>
+			</div>
+		</div>
 	);
 }
--- packages/firejs/src/jsx-runtime.ts
+++ packages/firejs/src/jsx-runtime.ts
@@ -1,1 +1,15 @@
-export { createElement } from "./index";
+import { createElement } from "./index";
+
+export { createElement };
+
+export function jsx(type, props, key) {
+  return createElement(type, { ...props, key });
+}
+
+export function jsxs(type, props, key) {
+  return createElement(type, { ...props, key });
+}
+
+export function jsxDEV(type, props, key, isStaticChildren, source, self) {
+  return createElement(type, { ...props, key });
+}
